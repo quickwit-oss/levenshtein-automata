ea28510e55c31f4b698cbfed579b8a55a7a77298
Paul Masurel <paul.masurel@gmail.com>
2017-11-19 17:49:47 +0900

    Simplified test code.

--- src/tests.rs
+++ src/tests.rs
@@ -34,61 +34,17 @@ fn test_levenshtein() {
     test_levenshtein_nfa_util("aab", "ab");
 }
 
-fn combinations(alphabet: &[char], len: usize) -> Vec<String> {
-    let mut result = vec![];
-    let mut prev: Vec<String> = vec![String::from("")];
-    for _ in 0..len {
-        prev = alphabet
-            .iter()
-            .cloned()
-            .flat_map(|letter: char| {
-                prev.iter().map(
-                    move |prefix| format!("{}{}", prefix, letter),
-                )
-            })
-            .collect();
-        result.extend_from_slice(&prev[..]);
-    }
-    result
-}
-
 #[test]
 #[ignore]
 fn test_levenshtein_nfa_slow() {
-    let test_sample = TestSample::with_num_chars(5);
+    let test_sample = TestSample::with_num_chars(5, "abcdef", true);
     test_sample.each(test_levenshtein_nfa_util);
-}
-
-fn generate_permutations(current: &mut [char], n: usize, dest: &mut Vec<Vec<char>>) {
-    if n == 1 {
-        dest.push(Vec::from(current));
-    } else {
-        for i in 0..n - 1 {
-            generate_permutations(current, n - 1, dest);
-            if n % 2 == 0 {
-                current.swap(i, n - 1);
-            } else {
-                current.swap(0, n - 1);
-            }
-        }
-        generate_permutations(current, n - 1, dest);
-    }
-}
-
-
-fn remap(mapping: &[char], text: &String) -> String {
-    let mut s = String::new();
-    for c in text.as_bytes() {
-        let i = c - 97;
-        s.push(mapping[i as usize]);
-    }
-    s
 }
 
 #[test]
 #[ignore]
 fn test_levenshtein_dfa_slow() {
-    let test_sample = TestSample::with_num_chars(5);
+    let test_sample = TestSample::with_num_chars(5, "あbぃaえ", false);
     let parametric_dfas: Vec<ParametricDFA> = (0u8..4u8)
         .map(|m| {
             let lev = LevenshteinNFA::levenshtein(m, false);
@@ -96,26 +52,17 @@ fn test_levenshtein_dfa_slow() {
         })
         .collect();
 
-    let mut alphabet = vec!['あ', 'b', 'ぃ', 'a', 'え'];
-    let mut alternate_mappings = Vec::new();
-    generate_permutations(&mut alphabet[..], 5, &mut alternate_mappings);
-
-    for mapping in &alternate_mappings {
-        for left in test_sample.lefts() {
-            let left = remap(mapping, &left);
-            for m in 0..4u8 {
-                let dfa = parametric_dfas[m as usize].build_dfa(&left);
-                for right in test_sample.rights() {
-                    let right = remap(mapping, &right);
-                    let expected = levenshtein::levenshtein(&left, &right) as u8;
-                    let expected_distance = make_distance(expected, m);
-                    let result_distance = dfa.eval(&right);
-                    assert_eq!(expected_distance, result_distance);
-                }
+    for left in test_sample.lefts() {
+        for m in 0..4u8 {
+            let dfa = parametric_dfas[m as usize].build_dfa(&left);
+            for right in test_sample.rights() {
+                let expected = levenshtein::levenshtein(&left, &right) as u8;
+                let expected_distance = make_distance(expected, m);
+                let result_distance = dfa.eval(&right);
+                assert_eq!(expected_distance, result_distance);
             }
         }
     }
-    test_sample.each(test_levenshtein_nfa_util);
 }
 
 #[test]
@@ -127,7 +74,7 @@ fn test_levenshtein_parametric_dfa_slow() {
             ParametricDFA::from_nfa(&lev)
         })
         .collect();
-    let test_sample = TestSample::with_num_chars(5);
+    let test_sample = TestSample::with_num_chars(5, "abcdef", true);
     test_sample.each(|left, right| {
         let expected = levenshtein::levenshtein(left, right) as u8;
         for m in 0u8..4u8 {
@@ -170,6 +117,23 @@ fn test_levenshtein_parametric_dfa_long() {
 
 }
 
+fn combinations(alphabet: &[char], len: usize) -> Vec<String> {
+    let mut result = vec![];
+    let mut prev: Vec<String> = vec![String::from("")];
+    for _ in 0..len {
+        prev = alphabet
+            .iter()
+            .cloned()
+            .flat_map(|letter: char| {
+                prev.iter().map(
+                    move |prefix| format!("{}{}", prefix, letter),
+                )
+            })
+            .collect();
+        result.extend_from_slice(&prev[..]);
+    }
+    result
+}
 
 struct TestSample {
     lefts: Vec<String>,
@@ -177,17 +141,19 @@ struct TestSample {
 }
 
 impl TestSample {
-    fn with_num_chars(num_chars: usize) -> TestSample {
-        let alphabet = vec!['a', 'b', 'c', 'd', 'e'];
+    fn with_num_chars(num_chars: usize, letters: &str, dedup_pattern: bool) -> TestSample {
+        let alphabet: Vec<char> = letters.chars().collect();
         let strings = combinations(&alphabet, num_chars);
         let sorted_strings: Vec<String> = strings
             .iter()
             .filter(|s| {
+                if !dedup_pattern {
+                    return true;
+                }
                 let mut v = HashSet::new();
-                for c in s.as_bytes() {
-                    if !v.contains(c) {
-                        let diff = (c - 97) as usize;
-                        if diff != v.len() {
+                for c in s.chars() {
+                    if !v.contains(&c) {
+                        if c != alphabet[v.len()] {
                             return false;
                         } else {
                             v.insert(c);
